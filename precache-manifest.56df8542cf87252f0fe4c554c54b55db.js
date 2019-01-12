1462c9ca9aac6a098337fab307b9edac6ca6d8a9
Cehhiro <hiro@cehh.io>
2019-01-12 04:54:57 -0600

    Updates

---
+++ precache-manifest.56df8542cf87252f0fe4c554c54b55db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b17af6d69ca2884c3d108599e5c42cac",
+    "url": "/static/media/logo.b17af6d6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "427c4eb18a14338449f7",
+    "url": "/static/js/main.427c4eb1.chunk.js"
+  },
+  {
+    "revision": "dd66b17623d62c4ba530",
+    "url": "/static/js/1.dd66b176.chunk.js"
+  },
+  {
+    "revision": "427c4eb18a14338449f7",
+    "url": "/static/css/main.bba20e65.chunk.css"
+  },
+  {
+    "revision": "dd66b17623d62c4ba530",
+    "url": "/static/css/1.77059a1c.chunk.css"
+  },
+  {
+    "revision": "65f44e704aafaadc74dd0560ec4e0e6f",
+    "url": "/index.html"
+  }
+];
